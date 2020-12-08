3429a81fb628c5bd89b26ee407785c97fbe8feee
ts 21 <shoukitsuda0310@gmail.com>
2020-12-09 03:46:15 +0900

    change .env setting

--- truffle-config.js
+++ truffle-config.js
@@ -23,38 +23,21 @@ dns.resolve(`ropsten.infura.io`, (error) => {
   console.error(error);
 });
 
+require('dotenv').config()
+
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-
-const fsKey = require('fs');
-const fsMne = require('fs');
-
-const infuraKey = fsKey.readFileSync(".infuraKey").toString().trim();
-const mnemonic = fsMne.readFileSync(".mnemonic").toString().trim();
+const MNEMONIC = process.env.MNEMONIC
+const ROPSTEN_URL = process.env.ROPSTEN_URL
 
 module.exports = {
-  /**
-   * Networks define how you connect to your ethereum client and let you set the
-   * defaults web3 uses to send transactions. If you don't  specify one truffle
-   * will spin up a development blockchain for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
-   * network from the command line, e.g
-   *
-   * $ truffle test --network <network-name>
-   */
-
   networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
-    ganache: {
-      host: "127.0.0.1",     // Localhost (default: none)
-      port: 7545,            // Standard Ethereum port (default: none)
-      gas: 6700000,
-      network_id: "5777",       // Any network (default: none)
-    },
+
+    // ganache: {
+    //   host: "127.0.0.1",     // Localhost (default: none)
+    //   port: 7545,            // Standard Ethereum port (default: none)
+    //   gas: 6700000,
+    //   network_id: "5777",       // Any network (default: none)
+    // },
 
     // Another network with more advanced options...
     // advanced: {
@@ -69,7 +52,7 @@ module.exports = {
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     ropsten: {
-      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraKey}`),
+      provider: () => new HDWalletProvider(MNEMONIC, ROPSTEN_URL),
       network_id: 3,       // Ropsten's id
       gas: 3000000,        // Ropsten has a lower block limit than mainnet
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
